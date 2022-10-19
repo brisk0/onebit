68d55c1cd8ceb0319055436cc78d644e202785a3
brisk <bskblake@gmail.com>
2022-10-19 17:21:15 +1030

    Output to stdout instead
    This is useful for concatenating files and makes fewer assumptions about
    intent

--- bit1.c
+++ bit1.c
@@ -9,33 +9,12 @@
 
 #define STBI_GREYSCALE 1
 
-// Swap an existing extension for ".c"
-// Caller must free the return value
-char *outfile(const char *infile) {
-	int n = strlen(infile);
-	const char *ext = ".c";
-	// We will append to the end if we don't find a final dot
-	int dotpos = n;
-	// Ignore initial dots so as to treat "dotfile" style names appropriately
-	for (int i = n-1; i > 0; i--) {
-		if (infile[i] == '.') {
-			dotpos = i;
-			break;
-		}
-	}
-	char *out = malloc(dotpos + strlen(ext) + 1);
-	memcpy(out, infile, dotpos);
-	memcpy(out + dotpos, ext, strlen(ext) + 1);
-	return out;
-}
-
 // Packs a buffer of 8 bytes into a single byte
 uint8_t pack(const unsigned char in[static 8]) {
 	uint8_t out = 0;
 	for (int i = 0; i < 8; i++) {
 		if (in[i]) {
 			out |= 1 << (7 - i);
-			//out |= 1 << i;
 		}
 	}
 	return out;
@@ -67,10 +46,6 @@ int main(int argc, char *argv[])
 		unsigned char *end = data + x*y;
 		unsigned char *p = begin;
 		unsigned char buffer[8] = {};
-		char *outname = outfile(*filename);
-		FILE *file = fopen(outname, "w");
-		free(outname);
-
 
 		for (int j = 0; j < finalbytes; j++) {
 			memset(buffer,0,8);
@@ -79,11 +54,9 @@ int main(int argc, char *argv[])
 				buffer[i] = *p;
 				p++;
 			}
-			fprintf(file, "%#x, ", pack(buffer));
+			printf("0x%.2X, ", pack(buffer));
 		}
-
-		// Write result to file with name from outfile
-		fclose(file);
+		printf("\n");
 
 		stbi_image_free(data);
 	}
